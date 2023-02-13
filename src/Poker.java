17fa9cd671bd3b1cbb6fbf8db62565a6ce8ab09c
Fekete Gergely <feketegergely062@gmail.com>
2023-02-13 16:45:11 +0100

    Kezdes1

--- src/Poker.java
+++ src/Poker.java
@@ -2,7 +2,13 @@ import java.util.Random;
 
 public class Poker {
 
-    public Poker() {
+    public Poker(){
+        osztas();
+        osztas();
+    }
+
+    
+    public void osztas(){
         System.out.printf(
             "%d %d %d %d %d\n", getPitch(), getPitch(), getPitch(), getPitch(), getPitch()
         );
